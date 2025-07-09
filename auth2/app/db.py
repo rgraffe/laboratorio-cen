9395c6c8efd73bf1b921264b738f0f8037779c91
AndresChacon00 <andresroberto.c@gmail.com>
2025-07-08 20:20:11 -0400

    auth backen en fastAPI

---
+++ auth2/app/db.py
@@ -0,0 +1,16 @@
+from sqlmodel import Session, SQLModel, create_engine
+import os
+from dotenv import load_dotenv
+load_dotenv()
+postgres_url = os.getenv(
+    "DATABASE_URL", "postgresql://username:password@localhost:5432/database_name"
+)
+print(postgres_url)
+engine = create_engine(postgres_url)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
